9272e851d4c5f6812518aa3dd294f2f5c1cf67eb
Astrid Tamara <astrid.tamara@hotmail.com>
2018-07-25 12:46:36 +0700

    Finish Day16

--- Day16/src/App.js
+++ Day16/src/App.js
@@ -1,123 +1,56 @@
 // @flow
 
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Image,
-  ScrollView
-} from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import type Route from './types/Route';
+
+import HomeScreen from './HomeScreen';
+import SearchScreen from './SearchScreen';
+import FavoriteScreen from './FavoriteScreen';
 
 type Props = {};
 
-type State = { value: string, links: Array<string>, favorites: Array<string> };
+type State = {currScreen: Route};
 
 export default class App extends Component<Props, State> {
   state = {
-    value: "apple",
-    links: [],
-    favorites: []
+    currScreen: 'HOME',
   };
-
-  async _fetchImage() {
-    let url = `https://api.imgur.com/3/gallery/search/?q=${this.state.value}`;
-    let result = await fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Client-ID 24b9bf1b56a95f7"
-      }
-    });
-    let res = await result.json();
-    let { data } = res;
-
-    let arrLinks = [];
-    data.forEach(({ link, is_album, images }) => {
-      if (!is_album) {
-        arrLinks.push(link);
-      }
-    });
-    this.setState({ links: arrLinks });
-  }
-
-  componentDidMount() {
-    this._fetchImage();
-  }
-
-  _onInputChange = (newText: string) => {
-    this.setState({ value: newText });
-    this._fetchImage();
-  };
+  favorites: Array<string> = [];
 
   render() {
-    let { value, links } = this.state;
-    let count = links.length;
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          value={value}
-          onChangeText={this._onInputChange}
+    let {currScreen} = this.state;
+    if (currScreen === 'HOME') {
+      return <HomeScreen navigateTo={this._navigateTo} />;
+    } else if (currScreen === 'SEARCH') {
+      return (
+        <SearchScreen
+          navigateTo={this._navigateTo}
+          setFavorite={this._setFavorite}
+          favorites={this.favorites}
         />
-        <Text style={styles.textBody}>
-          {value || "Please input some text."}
-        </Text>
-        <Text style={styles.textBody}>{count || "0"}</Text>
-        <ScrollView>
-          <View style={styles.imageContainer}>
-            {links.map((link, index) => {
-              return (
-                <Image
-                  style={styles.imgStyle}
-                  key={index}
-                  source={{ uri: link }}
-                />
-              );
-            })}
-          </View>
-        </ScrollView>
-      </View>
-    );
+      );
+    } else if (currScreen === 'FAVORITE') {
+      return (
+        <FavoriteScreen
+          navigateTo={this._navigateTo}
+          setFavorite={this._setFavorite}
+          favorites={this.favorites}
+        />
+      );
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    marginTop: 70
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#000",
-    paddingHorizontal: 4,
-    height: 22,
-    width: 200
-  },
-  textBody: {
-    paddingHorizontal: 4,
-    color: "#F0F"
-  },
-  imageContainer: {
-    display: "flex",
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "space-around",
-    margin: 0,
-    padding: 16
-  },
-  imgStyle: {
-    width: 100,
-    height: 100,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: "#000"
-  }
-});
+  _navigateTo = (newRoute: Route) => {
+    this.setState({currScreen: newRoute});
+  };
+
+  _setFavorite = (link: string) => {
+    let idx = this.favorites.indexOf(link);
+    if (idx === -1) {
+      this.favorites.push(link);
+    } else {
+      this.favorites.splice(idx, 1);
+    }
+  };
+}
